48038c239d554f3b295a4df022dc07d000e00409
Manish Verma <manish.verma@mintworxs.co>
2025-08-15 11:27:43 +0530

    update the mobile menu

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -65,25 +65,25 @@ export default function TopNavigations() {
           <div className="md:hidden bg-white border-t">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <a
-                href="#"
+                href="/"
                 className="text-gray-900 block px-3 py-2 text-base font-medium"
               >
                 Home
               </a>
               <a
-                href="#"
+                href="/"
                 className="text-gray-600 block px-3 py-2 text-base font-medium"
               >
                 Courses
               </a>
               <a
-                href="#"
+                href="/about-us"
                 className="text-gray-600 block px-3 py-2 text-base font-medium"
               >
                 About
               </a>
               <a
-                href="#"
+                href="/contact-us"
                 className="text-gray-600 block px-3 py-2 text-base font-medium"
               >
                 Contact
